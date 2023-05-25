8524c2caa0d09ff0ad3d8d44ced414c7ac00d366
svalino95 <santiago.valino@gmail.com>
2023-05-25 17:14:18 -0300

    chore: Refactorizar activity login

--- app/src/main/java/ort/tp3/cars/ui/views/LoginActivity.kt
+++ app/src/main/java/ort/tp3/cars/ui/views/LoginActivity.kt
@@ -1,10 +1,10 @@
-package ort.tp3.cars.activities
+package ort.tp3.cars.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ort.tp3.cars.R
 
-class Login_Activity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
