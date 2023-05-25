ff5f354b8aa2c2caeb902b5d0ab32c5294cb422d
svalino95 <santiago.valino@gmail.com>
2023-05-25 15:26:07 -0300

    feat: Agregar activity login

---
+++ app/src/main/java/ort/tp3/cars/activities/Login_Activity.kt
@@ -0,0 +1,12 @@
+package ort.tp3.cars.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ort.tp3.cars.R
+
+class Login_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
