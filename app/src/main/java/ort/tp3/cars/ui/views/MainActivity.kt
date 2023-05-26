0234d7b10e7a8b7cd5de4ddb3c832dc3c40cbe5c
jmostro <javiermondini@gmail.com>
2023-05-25 23:19:42 -0300

    feat: agregar drawer navigation

--- app/src/main/java/ort/tp3/cars/ui/views/MainActivity.kt
+++ app/src/main/java/ort/tp3/cars/ui/views/MainActivity.kt
@@ -1,12 +1,20 @@
 package ort.tp3.cars.ui.views
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.isVisible
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import ort.tp3.cars.R
 import ort.tp3.cars.adapters.CarsAdapter
@@ -14,17 +22,33 @@ import ort.tp3.cars.databinding.ActivityMainBinding
 import ort.tp3.cars.ui.viewmodels.CarsViewModel
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var binding: ActivityMainBinding
     private lateinit var recyclerView: RecyclerView
     private lateinit var carsAdapter: CarsAdapter
+    private lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
     private val carsViewModel: CarsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        drawer = findViewById(R.id.viewContainer)
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawer,
+            toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawer.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
 
         carsAdapter = CarsAdapter()
         recyclerView = findViewById(R.id.carsRecyclerView)
@@ -42,5 +66,35 @@ class MainActivity : AppCompatActivity() {
         })
 
         carsViewModel.onCreate()
+
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_item_profile -> Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_favorites -> Toast.makeText(this, "Favoritos", Toast.LENGTH_SHORT).show()
+            R.id.nav_item_settings -> Toast.makeText(this, "Configuración", Toast.LENGTH_SHORT)
+                .show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
