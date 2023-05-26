30cd719947a48dba6dcab94daa31fcfe51d7dca0
David Ezequiel Assen <assen.david@gmail.com>
2023-05-26 13:18:28 -0300

    chore: Refactorizar drawer navigation

--- app/src/main/java/ort/tp3/cars/ui/views/MainActivity.kt
+++ app/src/main/java/ort/tp3/cars/ui/views/MainActivity.kt
@@ -20,10 +20,10 @@ import com.google.android.material.navigation.NavigationView
 import ort.tp3.cars.ui.viewmodels.SharedViewModel
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener{
-
+class MainActivity : AppCompatActivity() {
     private lateinit var sharedViewModel: SharedViewModel
     private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var navigationView: NavigationView
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var drawer: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
@@ -31,11 +31,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         sharedViewModel = ViewModelProvider(this)[SharedViewModel::class.java]
         val email = intent.getStringExtra("username")
         val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
+
         drawer = findViewById(R.id.viewContainer)
+
         toggle = ActionBarDrawerToggle(
             this,
             drawer,
@@ -43,38 +46,27 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.string.navigation_drawer_open,
             R.string.navigation_drawer_close
         )
+
         drawer.addDrawerListener(toggle)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener(this)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        bottomNavView = findViewById(R.id.menu)
+        navigationView = findViewById(R.id.nav_view)
+
+        val navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+        NavigationUI.setupWithNavController(navigationView, navController)
 
         if (email != null) {
             sharedViewModel.setUsername(email)
         } else {
             sharedViewModel.setUsername("Usuario")
         }
-
-        navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
-
-        bottomNavView = findViewById(R.id.menu)
-
-        NavigationUI.setupWithNavController(bottomNavView, navHostFragment.navController)
-
-
     }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.nav_item_profile -> Toast.makeText(this, "Perfil", Toast.LENGTH_SHORT).show()
-            R.id.nav_item_favorites -> Toast.makeText(this, "Favoritos", Toast.LENGTH_SHORT).show()
-            R.id.nav_item_settings -> Toast.makeText(this, "Configuración", Toast.LENGTH_SHORT)
-                .show()
-        }
-        drawer.closeDrawer(GravityCompat.START)
-        return true
-    }
-
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
         toggle.syncState()
@@ -84,11 +76,11 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         super.onConfigurationChanged(newConfig)
         toggle.onConfigurationChanged(newConfig)
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (toggle.onOptionsItemSelected(item)){
+        if(toggle.onOptionsItemSelected(item)) {
             return true
         }
+
         return super.onOptionsItemSelected(item)
     }
 }
