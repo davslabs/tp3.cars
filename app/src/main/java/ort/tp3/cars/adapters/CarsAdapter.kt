52164d3e242cb47ba686debc9b9c4f3398c18330
svalino95 <santiago.valino@gmail.com>
2023-05-27 02:27:28 -0300

    Add ImageLogo to CardView

--- app/src/main/java/ort/tp3/cars/adapters/CarsAdapter.kt
+++ app/src/main/java/ort/tp3/cars/adapters/CarsAdapter.kt
@@ -4,6 +4,7 @@ import CarModel
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ort.tp3.cars.R
@@ -11,6 +12,8 @@ import ort.tp3.cars.R
 class CarsAdapter : RecyclerView.Adapter<CarsAdapter.CarsViewHolder>() {
     private var carsList: List<CarModel> = emptyList()
 
+
+   
     fun setCarsList(carsList: List<CarModel>) {
         this.carsList = carsList
         notifyDataSetChanged()
@@ -23,7 +26,6 @@ class CarsAdapter : RecyclerView.Adapter<CarsAdapter.CarsViewHolder>() {
 
     override fun onBindViewHolder(holder: CarsViewHolder, position: Int) {
         val car = carsList[position]
-
         holder.bind(car)
     }
 
@@ -32,13 +34,23 @@ class CarsAdapter : RecyclerView.Adapter<CarsAdapter.CarsViewHolder>() {
     }
 
     inner class CarsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val carLogoImageView: ImageView = itemView.findViewById(R.id.carLogoImageView)
         private val carNameTextView: TextView = itemView.findViewById(R.id.carNameTextView)
         private val carModelTextView: TextView = itemView.findViewById(R.id.carModelTextView)
         private val carYearTextView: TextView = itemView.findViewById(R.id.carYearTextView)
         fun bind(car: CarModel) {
+            carLogoImageView.setImageResource(getLogoResourceId(car.make))
             carNameTextView.text = car.make
             carModelTextView.text = car.model
             carYearTextView.text = car.year.toString()
+        }
+        private fun getLogoResourceId(brand: String): Int {
+            return when (brand) {
+                "bmw" -> R.drawable.logo_bmw
+                "volkswagen" -> R.drawable.logo_vw
+                "honda" -> R.drawable.logo_honda
+                else -> R.drawable.logo_toyota
+            }
         }
     }
 }
