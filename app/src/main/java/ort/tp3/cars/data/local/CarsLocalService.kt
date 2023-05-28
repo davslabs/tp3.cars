c4e6551e9784151202eea9865529b0003045ec70
svalino95 <santiago.valino@gmail.com>
2023-05-28 15:54:58 -0300

    Refactor URL imagenes en CarsLocalService.kt

--- app/src/main/java/ort/tp3/cars/data/local/CarsLocalService.kt
+++ app/src/main/java/ort/tp3/cars/data/local/CarsLocalService.kt
@@ -4,14 +4,17 @@ import ort.tp3.cars.dataclasses.BrandsModel
 import javax.inject.Inject
 
 class CarsLocalService @Inject constructor() {
-    suspend fun getAvailableBrands(): List<BrandsModel> {
+    private val logoBaseUrl1 = "https://www.carlogos.org/car-logos/"
+    private val logoBaseUrl2 = "https://www.carlogos.org/logo/"
+
+    fun getAvailableBrands(): List<BrandsModel> {
         return listOf(
-            BrandsModel("Maserati", "https://www.carlogos.org/car-logos/maserati-logo-2006-900x1200.png"),
-            BrandsModel("Mercedes", "https://www.carlogos.org/logo/Mercedes-Benz-logo-2011-1920x1080.png"),
-            BrandsModel("Audi", "https://www.carlogos.org/car-logos/audi-logo-2016.png"),
-            BrandsModel("Renault", "https://www.carlogos.org/logo/Renault-logo-2015-2048x2048.png"),
-            BrandsModel("Porsche", "https://www.carlogos.org/car-logos/porsche-logo-950x1100.png"),
-            BrandsModel("Jeep", "https://www.carlogos.org/car-logos/jeep-logo-1993.png"),
+            BrandsModel("Maserati", logoBaseUrl1 + "maserati-logo-2006-900x1200.png"),
+            BrandsModel("Mercedes", logoBaseUrl2 + "Mercedes-Benz-logo-2011-1920x1080.png"),
+            BrandsModel("Audi", logoBaseUrl1 + "audi-logo-2016.png"),
+            BrandsModel("Renault", logoBaseUrl2 + "Renault-logo-2015-2048x2048.png"),
+            BrandsModel("Porsche", logoBaseUrl1 + "porsche-logo-950x1100.png"),
+            BrandsModel("Jeep", logoBaseUrl1 + "jeep-logo-1993.png"),
         )
     }
 }
